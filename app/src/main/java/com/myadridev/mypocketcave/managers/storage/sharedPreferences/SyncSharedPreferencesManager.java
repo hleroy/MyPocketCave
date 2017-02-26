c45ac43e068ddeb8bcf065174c3cccc6f1479b39
adridevparis <mama0304>
2017-02-27 00:01:01 +0100

    Fix export.

--- app/src/main/java/com/myadridev/mypocketcave/managers/storage/sharedPreferences/SyncSharedPreferencesManager.java
+++ app/src/main/java/com/myadridev/mypocketcave/managers/storage/sharedPreferences/SyncSharedPreferencesManager.java
@@ -4,6 +4,8 @@ import android.os.Environment;
 
 import com.myadridev.mypocketcave.managers.storage.interfaces.ISyncStorageManager;
 
+import java.io.File;
+
 public class SyncSharedPreferencesManager implements ISyncStorageManager {
 
     public static SyncSharedPreferencesManager Instance;
@@ -12,7 +14,12 @@ public class SyncSharedPreferencesManager implements ISyncStorageManager {
     private String defaultLocation = null;
 
     private SyncSharedPreferencesManager() {
-        defaultLocation = Environment.getDownloadCacheDirectory().getAbsolutePath();
+        File externalStorageDirectory = Environment.getExternalStorageDirectory();
+        if (externalStorageDirectory != null) {
+            defaultLocation = externalStorageDirectory.getAbsolutePath();
+        } else {
+            defaultLocation = Environment.getDownloadCacheDirectory().getAbsolutePath();
+        }
     }
 
     public static void Init() {
